2b3f4472003f18bd949246f8b14bc2d84bde222d
RenzoQuispe <renzo123cd@gmail.com>
2025-06-16 09:56:53 -0500

    semana 1 eliminada

--- Semana1/DiccionariosEjercicios.py
+++
@@ -1,8 +0,0 @@
-#----------------------------------------------------------------------------------------------------------------
-
-# PRUEBAS y DEMOSTRACIONES
-# Problema 1
-conjunto1 = {"yellow", "black", "orange"}
-lista1 = ["blue", "green", "red"]
-conjunto1.update(lista1)
-print("Resultado Problema 1: ",conjunto1)
